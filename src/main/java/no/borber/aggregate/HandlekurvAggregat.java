649d4c024f16db232b83049abbaa4e5a21e5911c
Anders Karlsen <karlsenanders@gmail.com>
2014-03-15 15:38:44 +0100

    Aggregat tat hensyn til at event fjernes

--- src/main/java/no/borber/aggregate/HandlekurvAggregat.java
+++ src/main/java/no/borber/aggregate/HandlekurvAggregat.java
@@ -3,6 +3,7 @@ package no.borber.aggregate;
 import no.borber.monsterShop.monsterTypes.MonsterTypeJson;
 import no.border.eventstore.Event;
 import no.border.eventstore.EventSubscriber;
+import no.border.eventstore.KundenFjernetMonsterFraHandlekurven;
 import no.border.eventstore.KundenLaTilMonsterIHandlekurven;
 import no.no.borber.command.KundeLeggerMonsterIHandlekurven;
 
@@ -18,6 +19,10 @@ public class HandlekurvAggregat implements EventSubscriber {
         if (event instanceof KundenLaTilMonsterIHandlekurven){
             KundenLaTilMonsterIHandlekurven kundenLaTilMonsterIHandlekurven =(KundenLaTilMonsterIHandlekurven)event;
             handlekurven.add(kundenLaTilMonsterIHandlekurven.getMonster());
+        }
+        if (event instanceof KundenFjernetMonsterFraHandlekurven) {
+            KundenFjernetMonsterFraHandlekurven fjernetMonsterFraHandlekurven = (KundenFjernetMonsterFraHandlekurven) event;
+            handlekurven.remove(fjernetMonsterFraHandlekurven.getMonster());
         }
     }
 
