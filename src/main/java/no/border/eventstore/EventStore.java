6f63c9cd546a0d5dc4d15ca59f14d1d17a437663
Anders Karlsen <karlsenanders@gmail.com>
2014-03-15 11:54:56 +0100

    Adding event to eventstore

--- src/main/java/no/border/eventstore/EventStore.java
+++ src/main/java/no/border/eventstore/EventStore.java
@@ -1,16 +1,22 @@
 package no.border.eventstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by trefsahl on 15/03/14.
  */
 public class EventStore {
 
+    private List<Event> events = new ArrayList<>();
+
     public void addEvent(Event event) {
+        events.add(event);
 
     }
 
     public int size(){
-        return 0;
+        return events.size();
     }
 
 
