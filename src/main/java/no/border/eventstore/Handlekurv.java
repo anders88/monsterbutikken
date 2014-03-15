af9d715bfca88c741c365096690074c6818bf8a8
Anders Karlsen <karlsenanders@gmail.com>
2014-03-15 12:35:30 +0100

    Handlekurv teller antall items

--- src/main/java/no/border/eventstore/Handlekurv.java
+++ src/main/java/no/border/eventstore/Handlekurv.java
@@ -2,12 +2,14 @@ package no.border.eventstore;
 
 public class Handlekurv implements EventSubscriber {
 
+    int numberOfMonstersInBasket = 0;
+
     @Override
     public void eventAdded(Event event) {
-
+        numberOfMonstersInBasket++;
     }
 
     public int size() {
-        return 0;
+        return numberOfMonstersInBasket;
     }
 }
